66af66a94626da822fb3b3ecbc181832e2d3f2cd
Lucas Norris <luke.norris22@gmail.com>
2023-04-18 09:32:00 +1200

    added play button

--- src/Timer.js
+++ src/Timer.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import ProgressBar from '@ramonak/react-progress-bar'
+import PlayButton from './PlayButton'
+
+// const red = '#f54e4e'
+// const green = '#4aec8c'
 
 function Timer() {
   return (
     <div>
       <ProgressBar completed={60} />
+      <div style={{ marginTop: '20px' }}>
+        <PlayButton />
+      </div>
     </div>
   )
 }

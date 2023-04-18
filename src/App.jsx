28979cecb9e8305329abba962a8cd9339115eb49
Lucas Norris <luke.norris22@gmail.com>
2023-04-18 16:06:38 +1200

    add SettingsContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Timer from './Timer'
 import Settings from './Settings'
+import SettingsContext from './SettingsContext'
 
 function App() {
   const [showSettings, setShowSettings] = useState(true)
 
-  return <main>{showSettings ? <Settings /> : <Timer />}</main>
+  return (
+    <main>
+      <SettingsContext.Provider
+        value={{
+          workMinutes: 25,
+          breakMinutes: 15,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}\
+      </SettingsContext.Provider>
+    </main>
+  )
 }
 
 export default App
---
+++ src/SettingsContext.jsx
@@ -0,0 +1,6 @@
+import react from 'react'
+
+
+const SettingsContext = react.createContext(defaultValue: {})
+
+export default SettingsContext

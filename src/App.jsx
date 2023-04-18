0de3124f37dd3729ce3b7e539a328c12c5fe8324
Lucas Norris <luke.norris22@gmail.com>
2023-04-18 14:44:21 +1200

    add slider styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Timer from './Timer'
 import Settings from './Settings'
 
 function App() {
-  const [showSettings, setShowSettings] = useState(false)
+  const [showSettings, setShowSettings] = useState(true)
 
   return <main>{showSettings ? <Settings /> : <Timer />}</main>
 }
--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,5 +1,29 @@
+import ReactSlider from 'react-slider'
+import './slider.css'
+
 function Settings() {
-  return <div>Settings</div>
+  return (
+    <div style={{ textAlign: 'left' }}>
+      <label>work minutes:</label>
+      <ReactSlider
+        className={'slider'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        value={45}
+        min={1}
+        max={120}
+      />
+      <label>break minutes:</label>
+      <ReactSlider
+        className={'slider green'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        value={45}
+        min={1}
+        max={120}
+      />
+    </div>
+  )
 }
 
 export default Settings

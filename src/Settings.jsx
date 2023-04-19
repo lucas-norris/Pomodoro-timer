21492317285ce714447b17d5e86ca87c85dbdb90
Lucas Norris <luke.norris22@gmail.com>
2023-04-20 10:56:54 +1200

    add onclick to back button component

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -30,7 +30,11 @@ function Settings() {
         max={120}
       />
       <div style={{ textAlign: 'center', marginTop: '20px' }}>
-        <BackButton />
+        <BackButton
+          onClick={() => {
+            settingsInfo.setShowSettings(false)
+          }}
+        />
       </div>
     </div>
   )
